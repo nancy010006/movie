d696e175982f9eb349d07b5b7977e46c3d04ef9a
nancy <denny512369874@gmail.com>
2023-08-19 21:02:19 +0800

    在影片播放頁面使用video.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,13 @@ window.Vue = require('vue').default;
 
 Vue.component('home-page', require('./components/HomePage.vue').default);
 
+import Vue from 'vue';
+import VideoPlayer from 'vue-video-player';
+// 需要 `video-player` 的樣式
+import 'video.js/dist/video-js.css';
+
+Vue.use(VideoPlayer);
+
 const app = new Vue({
     el: '#app',
     router, // 將路由注入 Vue 實例
--- resources/js/components/MainVideo.vue
+++ resources/js/components/MainVideo.vue
@@ -1,8 +1,44 @@
 <template>
   <div class="main-video">
-    <video controls width="100%">
-      <source src="path/to/your/video.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+    <video ref="videoPlayer" class="video-js" controls></video>
   </div>
 </template>
+
+<script>
+import 'video.js/dist/video-js.css';
+import videojs from 'video.js';
+
+export default {
+  mounted() {
+    const player = videojs(this.$refs.videoPlayer, {
+      controls: true,
+      sources: [
+        { type: 'video/mp4', src: 'http://local.resources/videos/uccu0323_3172327330899320080.mp4' }
+      ]
+    });
+
+    // 添加鍵盤快捷鍵事件
+    player.on('keydown', (e) => {
+      if (e.key === 'ArrowRight') {
+        player.currentTime(player.currentTime() + 5);
+      } else if (e.key === 'ArrowLeft') {
+        player.currentTime(player.currentTime() - 5);
+      }else if (e.code === 'Space') { // 空白鍵控制播放和暫停
+        if (player.paused()) {
+          player.play();
+        } else {
+          player.pause();
+        }
+      } else if (e.code === 'ArrowUp') { // 上箭頭鍵控制增加音量
+        player.volume(Math.min(player.volume() + 0.1, 1));
+      } else if (e.code === 'ArrowDown') { // 下箭頭鍵控制減小音量
+        player.volume(Math.max(player.volume() - 0.1, 0));
+      }
+    });
+  }
+};
+</script>
+
+<style>
+/* 如果需要，可以在這裡添加一些樣式 */
+</style>
