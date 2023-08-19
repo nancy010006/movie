33b405e7f3bf996320676a27020d0889fb3bb19d
nancy <denny512369874@gmail.com>
2023-08-19 21:56:05 +0800

    將首頁的預覽換成影片預覽

--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -4,8 +4,17 @@
     <div class="movies">
       <div v-if="loading">Loading movies...</div>
       <div class="movie-card" v-for="movie in movies" :key="movie.id">
-        <router-link :to="`/video/${encodeURIComponent(movie.video_path)}`"> <!-- 使用影片的 path -->
-          <img v-if="movie.previews && movie.previews.length > 0" :src="movie.previews[0].image_path" alt="Movie poster" class="movie-image" />
+        <router-link :to="`/video/${encodeURIComponent(movie.video_path)}`">
+          <video
+            ref="previewVideo"
+            v-if="movie.previews && movie.previews.length > 0"
+            :src="movie.video_path"
+            class="movie-video"
+            @mouseover="playPreview"
+            @mouseout="pausePreview"
+            loop
+            muted
+          ></video>
           <h3 class="movie-title">{{ movie.title }}</h3>
         </router-link>
       </div>
@@ -39,10 +48,27 @@ export default {
         this.loading = false;
       });
   },
+  methods: {
+    playPreview(event) {
+      const videoElement = event.target;
+      videoElement.currentTime = videoElement.duration / 2; // 將播放位置設置到影片中間
+      videoElement.play(); // 播放影片
+    },
+    pausePreview(event) {
+      event.target.pause(); // 暫停影片
+      event.target.currentTime = 0; // 將播放位置重置到開始
+    }
+  }
 };
 </script>
 
 <style scoped>
+
+.movie-video {
+  width: 100%;
+  height: auto;
+  cursor: pointer;
+}
 
 .movies {
   display: flex;
