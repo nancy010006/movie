af1a06413db5df88c2b00fe81773bdfc51122c02
nancy <denny512369874@gmail.com>
2023-08-20 16:36:45 +0800

    引入vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,9 +10,12 @@ import VideoPlayer from 'vue-video-player';
 // 需要 `video-player` 的樣式
 import 'video.js/dist/video-js.css';
 
+import store from './store'; // 確保路徑正確指向你的 store.js 文件
+
 Vue.use(VideoPlayer);
 
 const app = new Vue({
     el: '#app',
+    store, // 加入 store
     router, // 將路由注入 Vue 實例
 });
