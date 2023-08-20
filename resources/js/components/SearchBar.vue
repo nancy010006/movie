82918d37ebdf6d6f4dd6975a0c81b56bf1bbe54f
nancy <denny512369874@gmail.com>
2023-08-20 16:39:49 +0800

    將searchBar的資料使用vuex控制

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -5,6 +5,31 @@
   </div>
 </template>
 
+<script>
+export default {
+  computed: {
+    query: {
+      get() {
+        return this.$store.state.query; // 從 Vuex 獲取查詢
+      },
+      set(value) {
+        this.$store.commit('updateQuery', value); // 將查詢保存到 Vuex
+      }
+    }
+  },
+  watch: {
+    '$route.query.query': function(newVal) {
+      this.query = newVal || ''; // 更新查詢字串，如果路由變更
+    }
+  },
+  methods: {
+    searchMovies() {
+      this.$router.push({ name: 'SearchResults', query: { query: this.query } });
+    },
+  },
+};
+</script>
+
 <style scoped>
   .search-box {
     width: 300px;
@@ -23,18 +48,3 @@
     margin-bottom: 20px;
   }
 </style>
-
-<script>
-export default {
-  data() {
-    return {
-      query: '',
-    };
-  },
-  methods: {
-    searchMovies() {
-      this.$router.push({ name: 'SearchResults', query: { query: this.query } });
-    },
-  },
-};
-</script>
