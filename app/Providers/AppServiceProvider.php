92582788f6bb445e8b99c7e9e0a9801b0b5d54ae
nancy <denny512369874@gmail.com>
2023-08-20 11:20:28 +0800

    安裝elasticsearch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Elasticsearch\ClientBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('elasticsearch', function ($app) {
+            return ClientBuilder::create()
+                ->setHosts(config('elasticsearch.hosts'))
+                ->build();
+        });
     }
 
     /**
