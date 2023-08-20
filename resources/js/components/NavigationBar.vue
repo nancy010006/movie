e266a5b4d45a210c226106b2437243c1ff77e5da
nancy <denny512369874@gmail.com>
2023-08-20 16:57:22 +0800

    實作導覽列

---
+++ resources/js/components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<!-- NavigationBar.vue -->
+<template>
+  <div class="navigation">
+    <a @click="goToHomePage" class="home-button">首頁</a>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToHomePage() {
+      this.$store.commit('clearQuery'); // 使用 Vuex 的 mutation 清除查詢
+      this.$router.push({ name: 'HomePage' }); // 使用具名路由跳轉到首頁
+    }
+  }
+};
+</script>
+
+<style>
+  .home-button {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    padding: 10px;
+    font-size: 16px;
+    cursor: pointer;
+    text-decoration: none;
+    color: #000;
+  }
+</style>
